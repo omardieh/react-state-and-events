608e7d537aeba17a7e98a3838bf4e6c1a23f34aa
Omar Sh. Dieh <omarsheikhdieh@gmail.com>
2023-07-11 12:53:21 +0200

    conditional render

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import SimpleList from "./components/SimpleList";
 import StudentList from "./components/StudentList";
 import ProjectList from "./components/ProjectList";
 import MovieList from "./components/MovieList";
+import { useState } from "react";
+import Spinner from "./components/Spinner";
+import ImprovedMovieList from "./components/ImprovedMovieList";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  if (isLoading) {
+    return <Spinner />;
+  }
   return (
     <div className={"App"}>
       {/* <SimpleList /> */}
       {/* <StudentList /> */}
       {/* <ProjectList /> */}
-      <MovieList />
+      {/* <MovieList /> */}
+      <ImprovedMovieList />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,17 @@
 function MovieCard(props) {
+  // JS here
   const { movie, handleDelete } = props;
 
   return (
+    // JSX here
     <div className="MovieCard">
       <h3>{movie.title}</h3>
       <p>Director: {movie.director}</p>
       <p>Rating: {movie.IMDBRating}</p>
-      {/* Keep the button commented out for now */}
+      {/* {movie.hasOscars && <p> Got an Oscar! </p>}
+      {!movie.hasOscars && <p> No Oscar </p>} */}
+
+      {movie.hasOscars ? <p> Got an Oscar! </p> : <p> No Oscar </p>}
       <button onClick={() => handleDelete(movie._id)} className="btn-delete">
         Delete
       </button>
